ea30208d6fff3d03a1262f5f516f3b97dcb4d14c
Lucas Nascimento <contactlucasnascimento@gmail.com>
2024-09-20 11:33:18 -0300

    Add files via upload

---
+++ Teste.java
@@ -0,0 +1,29 @@
+package br.ucsal.edu;
+
+import java.util.Scanner;
+
+public class Teste {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite o primeiro número");
+		
+		int primeiroNumero;
+		primeiroNumero = sc.nextInt();
+		
+		System.out.println("Digite o segundo número");
+		
+		int segundoNumero;
+		segundoNumero = sc.nextInt();
+		
+		System.out.println(primeiroNumero + " + " + segundoNumero + " = " + (primeiroNumero+segundoNumero));
+		System.out.println(primeiroNumero + " - " + segundoNumero + " = " + (primeiroNumero-segundoNumero));
+		System.out.println(primeiroNumero + " * " + segundoNumero + " = " + (primeiroNumero*segundoNumero));
+		System.out.println(primeiroNumero + " / " + segundoNumero + " = " + (primeiroNumero/segundoNumero));
+		System.out.println(primeiroNumero + " mod " + segundoNumero + " = " + (primeiroNumero%segundoNumero));
+		
+		
+	}
+
+}
